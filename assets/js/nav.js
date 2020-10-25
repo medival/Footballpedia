783d6890b866859c5a1231578e468dfbb66fa022
Adi Purnomo <adipurnomo811@gmail.com>
2020-10-26 05:33:38 +0700

    Add  page

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -29,4 +29,48 @@ document.addEventListener("DOMContentLoaded", function () {
         xhttp.open("GET", "nav.html", true);
         xhttp.send();
     }
+
+    let page = window.location.hash.substr(1);
+    if (page === "") page = "uefa";
+    loadPage(page);
+
+    function loadPage(page){
+        let xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (this.readyState === 4) {
+                let content = document.querySelector("#body-content");
+
+                switch (page) {
+                    case "fifa":
+                     getTeams(2000);
+                     break;
+
+                    case "uefa":
+                     getTeams(2001);
+                     break;
+                    
+                    case "favorite":
+                     getFavoriteTeams();
+                     break;
+
+                    default:
+                     getTeams(2001);
+                     break;
+                }
+
+                if (this.status == 200) {
+                    content.innerHTML = xhttp.responseText;
+                } else if (this.status == 404) {
+                    content.innerHTML = "<p> Page you are looking for is missing </p>";
+                } else {
+                    content.innerHTML = "<p> Ops! Check your WiFi</p>";
+                    M.toast({
+                        html: `Damn! API is limited`
+                    })
+                }
+            }
+        };
+        xhttp.open("GET", "pages/home.html", TRUE);
+        xhttp.send();
+    }
 })
