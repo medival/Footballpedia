0f681f38b277fb5a21ad119842ed3d78feb826a4
Adi Purnomo <adipurnomo811@gmail.com>
2020-10-30 01:57:57 +0700

    ui

--- assets/js/api.js
+++ assets/js/api.js
@@ -99,7 +99,7 @@ function getTeamById() {
                     response.json()
                     .then((data) => {
                         
-                        changeTeamData(data);
+                        teamInfo(data);
 
                         showClubMember(data);
                         resolve(data);
@@ -120,7 +120,7 @@ function getTeamById() {
         .then(status)
         .then(json)
         .then((data) => {
-            changeTeamData(data);
+            teamInfo(data);
 
             showClubMember(data);
             resolve(data);
@@ -184,11 +184,38 @@ function getSavedTeamById() {
 
     getById(idParam)
     .then((team) => {
-        console.log(team);
-        changeTeamData(team);
+        teamInfo(data);
         showClubMember(team);
     });
     return idParam;
+}
+
+function teamInfo(data) {
+    console.log('Daatanya');
+    console.log(data);
+    let teamInfoHTML = `
+        <div class="col s5 teamLeft center">
+            <img src="${data.crestUrl}" alt="${data.name}" srcset="" class="responsive-img">
+            <h5 class="founded">${data.founded}</h5>
+        </div>
+        <div class="col s7">
+            <h4 class="team-heading"> ${data.name} </h4>
+            <div class="row">
+                <div class="col s1"><i class="material-icons">place</i></div>
+                <div class="col">${data.address}</div>
+            </div>
+            <div class="row">
+                <div class="col s1"> <i class="material-icons">phone</i></div>
+                <div class="col">${data.phone}</div>
+            </div>
+            <div class="row">
+                <div class="col s1"><i class="material-icons">language</i></div>
+                <div class="col"> <a href="${data.website}" target="_blank">${data.website}</a></div>
+                </div>
+        </div>
+        `;
+    
+    document.getElementById("team-info").innerHTML = teamInfoHTML;
 }
 
 function changeLeague(data) {
@@ -239,34 +266,43 @@ function changeTeamData(data) {
     // teamVenueElement.innerText = teamVenue;
 }
 
+// Convert Date to Age
+const getAge = birthDate => Math.floor((new Date() - new Date(birthDate).getTime()) / 3.15576e+10);
+
 function showClubMember(data) {
+    console.log(data);
     let squadHTML = `
-        <div class="row">
-            <div class="col s1"><b>#</b></div>
-            <div class="col s3"><b>Name</b></div>
-            <div class="col s3"><b>Position</b></div>
-            <div class="col s3"><b>Nationality</b></div>
-            <div class="col s2"><b>Role</b></div>
-        </div>
+        <thead>
+            <tr>
+                <th> # </th>
+                <th> Name </th>
+                <th> Position </th>
+                <th> Age </th>
+                <th> Nationatlity </th>
+                <th> Role </th>
+            </th>
+        </thead>
+        <tbody>
     `;
     data.squad.forEach(player => {
-        let number = "";
+        let shirtNumber = "";
         let position = "";
 
-        (player.number === null) ? (number = "-") : (number = player.number);
-        (player.position === null) ? (position = "-") : (position = player.position);
+        (player.shirtNumber == null) ? (shirtNumber = "-") : (shirtNumber = player.shirtNumber);
+        (player.position == null) ? (position = "-") : (position = player.position);
 
         squadHTML += `
-            <div class="row">
-                <div class="col s1">${number}</div>
-                <div class="col s3">${player.name}</div>
-                <div class="col s3">${player.position}</div>
-                <div class="col s3">${player.nationality}</div>
-                <div class="col s2">${player.role}</div>
-            </div>
+            <tr>
+                <td>${shirtNumber}</td>
+                <td>${player.name}</td>
+                <td>${position}</td>
+                <td>${getAge(player.dateOfBirth)}</td>
+                <td>${player.nationality}</td>
+                <td>${player.role}</td>
+            </tr>
         `;
     });
-
+    squadHTML += `</tbody>`;
     document.getElementById("squadPlayer").innerHTML = squadHTML;
 }
 
@@ -279,15 +315,15 @@ function inputCards(team) {
         <div class="col s12 m6 l3 card-wrapper">
             <div class="club-wrapper">
                 <div class="club-info">
-                    <a href="./team.html?id=${team.id}">
-                        <div class="club-image waves-effect waves-block waves-light">
-                            <img src="${src.replace(/^http:\/\//i, 'https://')}" class="responsive-img" alt="${team.shortName}" title="${team.shortName}">
-                        </div>
-                    </a>
                     <div class="card-content center-align">
-                        <h6>${team.shortName}</h6>
+                        <a href="./team.html?id=${team.id}">
+                            <div class="club-image waves-effect waves-block waves-light">
+                                <img src="${src.replace(/^http:\/\//i, 'https://')}" class="responsive-img" alt="${team.shortName}" title="${team.shortName}">
+                            </div>
+                        </a>
                     </div>
-                    <div class="card-action">
+                    <div class="card-action center-align">
+                        <h6>${team.shortName}</h6>
                     </div>
                 </div>
             </div>
