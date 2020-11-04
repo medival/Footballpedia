8c2e46373c6da880ed9bc6c9c76f8917abb75297
Adi Purnomo <adipurnomo811@gmail.com>
2020-11-04 09:17:14 +0700

    Add using workbox

--- service-worker.js
+++ service-worker.js
@@ -1,109 +1,129 @@
-const CACHE_NAME = "Footballpedia-v1";
-const urlsToCache = [
-	"/",
-	"/favicon.ico",
-	"/index.html",
-	"/manifest.json",
-	"/push.js",
-	"/service-worker.js",
-	"/assets/css/font-roboto.css",
-	"/assets/css/material-icons.css",
-	"/assets/css/materialize.min.css",
-	"/assets/css/preloader.css",
-	"/assets/css/style.css",
-	"/assets/css/sweetalert2.min.css",
-	"/assets/fonts/Roboto/KFOmCnqEu92Fr1Mu4mxKKTU1Kg.woff2",
-	"/assets/fonts/Roboto/KFOmCnqEu92Fr1Mu4WxKKTU1Kvnz.woff2",
-	"/assets/fonts/Roboto/KFOmCnqEu92Fr1Mu5mxKKTU1Kvnz.woff2",
-	"/assets/fonts/Roboto/KFOmCnqEu92Fr1Mu7GxKKTU1Kvnz.woff2",
-	"/assets/fonts/Roboto/KFOmCnqEu92Fr1Mu7WxKKTU1Kvnz.woff2",
-	"/assets/fonts/Roboto/KFOmCnqEu92Fr1Mu72xKKTU1Kvnz.woff2",
-	"/assets/fonts/MaterialIcons-Regular.eot",
-	"/assets/fonts/MaterialIcons-Regular.ttf",
-	"/assets/fonts/MaterialIcons-Regular.woff",
-	"/assets/fonts/MaterialIcons-Regular.woff2",
-	"/assets/images/icons/favicon-72x72.png",
-	"/assets/images/icons/favicon-96x96.png",
-	"/assets/images/icons/favicon-144x144.png",
-	"/assets/images/icons/favicon-152x152.png",
-	"/assets/images/icons/favicon-192x192.png",
-	"/assets/images/icons/favicon-512x512.png",
-	"/assets/js/api.js",
-	"/assets/js/db.js",
-	"/assets/js/idb.js",
-	"/assets/js/index.js",
-	"/assets/js/jquery-3.4.1.min.js",
-	"/assets/js/materialize.min.js",
-	"/assets/js/nav.js",
-	"/assets/js/preloader.js",
-	"/assets/js/sweetalert2.all.min.js",
-	"/assets/js/sweetalert2.min.js",
-	"/assets/js/team.js",
-	"/pages/home.html",
-	"/pages/nav.html",
-	"/pages/team.html",
-];
+importScripts(
+	"https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js"
+);
 
-self.addEventListener("install", function (event) {
-	event.waitUntil(
-		caches.open(CACHE_NAME).then(function (cache) {
-			return cache.addAll(urlsToCache);
+if (workbox) {
+	console.log("workbox has been loaded");
+	workbox.precaching.precacheAndRoute([
+		{ url: "/", revision: "2" },
+		{ url: "/favicon.ico", revision: "2" },
+		{ url: "/index.html", revision: "2" },
+		{ url: "/manifest.json", revision: "2" },
+		{ url: "/push.js", revision: "2" },
+		{ url: "/service-worker.js", revision: "2" },
+		{ url: "/assets/css/font-roboto.css", revision: "2" },
+		{ url: "/assets/css/material-icons.css", revision: "2" },
+		{ url: "/assets/css/materialize.min.css", revision: "2" },
+		{ url: "/assets/css/preloader.css", revision: "2" },
+		{ url: "/assets/css/style.css", revision: "2" },
+		{ url: "/assets/css/sweetalert2.min.css", revision: "2" },
+		{
+			url: "/assets/fonts/Roboto/KFOmCnqEu92Fr1Mu4mxKKTU1Kg.woff2",
+			revision: "2",
+		},
+		{
+			url: "/assets/fonts/Roboto/KFOmCnqEu92Fr1Mu4WxKKTU1Kvnz.woff2",
+			revision: "2",
+		},
+		{
+			url: "/assets/fonts/Roboto/KFOmCnqEu92Fr1Mu5mxKKTU1Kvnz.woff2",
+			revision: "2",
+		},
+		{
+			url: "/assets/fonts/Roboto/KFOmCnqEu92Fr1Mu7GxKKTU1Kvnz.woff2",
+			revision: "2",
+		},
+		{
+			url: "/assets/fonts/Roboto/KFOmCnqEu92Fr1Mu7WxKKTU1Kvnz.woff2",
+			revision: "2",
+		},
+		{
+			url: "/assets/fonts/Roboto/KFOmCnqEu92Fr1Mu72xKKTU1Kvnz.woff2",
+			revision: "2",
+		},
+		{ url: "/assets/fonts/MaterialIcons-Regular.eot", revision: "2" },
+		{ url: "/assets/fonts/MaterialIcons-Regular.ttf", revision: "2" },
+		{ url: "/assets/fonts/MaterialIcons-Regular.woff", revision: "2" },
+		{ url: "/assets/fonts/MaterialIcons-Regular.woff2", revision: "2" },
+		{ url: "/assets/images/icons/favicon-72x72.png", revision: "2" },
+		{ url: "/assets/images/icons/favicon-96x96.png", revision: "2" },
+		{ url: "/assets/images/icons/favicon-144x144.png", revision: "2" },
+		{ url: "/assets/images/icons/favicon-152x152.png", revision: "2" },
+		{ url: "/assets/images/icons/favicon-192x192.png", revision: "2" },
+		{ url: "/assets/images/icons/favicon-512x512.png", revision: "2" },
+		{ url: "/assets/js/api.js", revision: "2" },
+		{ url: "/assets/js/db.js", revision: "2" },
+		{ url: "/assets/js/idb.js", revision: "2" },
+		{ url: "/assets/js/index.js", revision: "2" },
+		{ url: "/assets/js/jquery-3.4.1.min.js", revision: "2" },
+		{ url: "/assets/js/materialize.min.js", revision: "2" },
+		{ url: "/assets/js/nav.js", revision: "2" },
+		{ url: "/assets/js/preloader.js", revision: "2" },
+		{ url: "/assets/js/sweetalert2.all.min.js", revision: "2" },
+		{ url: "/assets/js/sweetalert2.min.js", revision: "2" },
+		{ url: "/assets/js/team.js", revision: "2" },
+		{ url: "/pages/home.html", revision: "2" },
+		{ url: "/pages/nav.html", revision: "2" },
+		{ url: "/pages/team.html", revision: "2" },
+	]);
+
+	// API Football
+	workbox.routing.registerRoute(
+		new RegExp("https://api.football-data.org/v2/"),
+		workbox.strategies.staleWhileRevalidate({
+			cacheName: "api-football",
 		})
 	);
-});
+	// End of API Football
 
-self.addEventListener("fetch", function (event) {
-	let baseUrl = "https://api.football-data.org/v2/";
-	if (event.request.url.indexOf(baseUrl) > -1) {
-		event.respondWith(
-			caches.open(CACHE_NAME).then(function (cache) {
-				return fetch(event.request).then(function (response) {
-					cache.put(event.request.url, response.clone());
-					return response;
-				});
-			})
-		);
-	} else {
-		event.respondWith(
-			caches
-				.match(event.request, {
-					ignoreSearch: true,
-				})
-				.then((response) => {
-					return response || fetch(event.request);
-				})
-		);
-	}
-});
-
-self.addEventListener("activate", function (event) {
-	event.waitUntil(
-		caches.keys().then(function (cacheNames) {
-			return Promise.all(
-				cacheNames.map(function (cacheName) {
-					if (cacheName != CACHE_NAME) {
-						console.log(
-							`ServiceWorker: cache ${cacheName} deleted`
-						);
-						return cache.delete(cacheName);
-					}
-				})
-			);
+	// Cache Pages
+	workbox.routing.registerRoute(
+		new RegExp("/pages/"),
+		workbox.strategies.staleWhileRevalidate({
+			cacheName: "pages",
 		})
 	);
-});
+	// End of Cache Pages
 
-self.addEventListener("push", function (event) {
+	// Cache Images
+	workbox.routing.registerRoute(
+		/.*(?:png|gif|jpg|jpeg|svg)$/,
+		workbox.strategies.cacheFirst({
+			cacheName: "image",
+			plugins: [
+				new workbox.cacheableResponse.Plugin({
+					statuses: [0, 200],
+				}),
+				new workbox.expiration.Plugin({
+					maxEntries: 100,
+					maxAgeSeconds: 30 * 24 * 60 * 60,
+				}),
+			],
+		})
+	);
+	// End of Cache Images
+
+	// Cache Assets JS,CSS
+	workbox.routing.registerRoute(
+		/\.(?:js|css)$/,
+		workbox.strategies.cacheFirst({
+			cacheName: "static-resources",
+		})
+	);
+	// Cache Assets JS,CSS
+} else {
+	console.log(`Workbox fail to load`);
+}
+self.addEventListener("push", (event) => {
 	let body;
 	if (event.data) {
 		body = event.data.text();
 	} else {
 		body = "Push message no payload";
 	}
-
-	let options = {
+	const options = {
 		body: body,
-		icon: "img/notification.png",
+		icon: "/assets/images/icons/favicon-512x512.png",
 		vibrate: [100, 50, 100],
 		data: {
 			dateOfArrival: Date.now(),
@@ -111,6 +131,6 @@ self.addEventListener("push", function (event) {
 		},
 	};
 	event.waitUntil(
-		self.registration.showNotification("Push Notificaton", options)
+		self.registration.showNotification("Push Notification", options)
 	);
 });
